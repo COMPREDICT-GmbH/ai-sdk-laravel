7ea2f09760a8ef8c4af283d1687090d87939835f
Ousama Esbel <esbel@compredict.de>
2019-11-14 10:19:23 +0100

    Add the ability to change the SDK url for development purposes

--- src/models/CPAlgorithmTrait.php
+++ src/models/CPAlgorithmTrait.php
@@ -20,6 +20,8 @@ trait CPAlgorithmTrait
         if (is_null($this->algorithms) && is_null($this->client)) {
             $callback_url = \Config::get('compredict.ai_core.callback');
             $this->client = new CPAlgoClient($this->APIKey, $callback_url);
+            // Get the URL if found in env. Otherwise, get default url from client
+            $this->client->setURL(env('COMPREDICT_SERVER_URL', $this->client->getURL()));
             $this->algorithms = $this->client->getAlgorithms()->algorithms;
         }
         return $this->algorithms;
