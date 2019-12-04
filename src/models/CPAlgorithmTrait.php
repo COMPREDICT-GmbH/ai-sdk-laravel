21550cb8a38baa1f42a33d31e602b0f691aa85ba
Ousama Esbel <esbel@compredict.de>
2019-12-04 09:48:40 +0100

    Make the attribute iteratable and algorithms as a function return an instance

--- src/models/CPAlgorithmTrait.php
+++ src/models/CPAlgorithmTrait.php
@@ -12,7 +12,7 @@ trait CPAlgorithmTrait
 
     public function getAlgorithmsAttribute()
     {
-        return $this->algorithms;
+        return $this->algorithms->algorithms;
     }
 
     public function algorithms()
@@ -22,7 +22,7 @@ trait CPAlgorithmTrait
             $this->client = new CPAlgoClient($this->APIKey, $callback_url);
             // Get the URL if found in env. Otherwise, get default url from client
             $this->client->setURL(env('COMPREDICT_SERVER_URL', $this->client->getURL()));
-            $this->algorithms = $this->client->getAlgorithms()->algorithms;
+            $this->algorithms = $this->client->getAlgorithms();
         }
         return $this->algorithms;
     }
